0da17cdfb7cd4947cea372119265955a2c9e46ee
viktorkulygin <mrastonone@gmail.com>
2023-05-21 16:42:31 +0300

    move to typescript

--- t2-dashboard-api/index.ts
+++ t2-dashboard-api/index.ts
@@ -1,21 +1,21 @@
-import express from 'express';
+import express, {Express, NextFunction, Request, Response} from 'express';
 import {userRouter} from './users/users.js';
 
-const port = 8000;
-const app = express();
+const port: number = 8000;
+const app: Express = express();
 
-app.use((req, res, next) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
     console.log('Request time for main: ', new Date());
     next();
 });
 
-app.get('/hello', (req, res) => {
+app.get('/hello', (req: Request, res: Response) => {
     throw new Error('My error!');
 });
 
 app.use('/users', userRouter);
 
-app.use((err, req, res, next) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     console.log(err.message);
     res.status(400).send(err.message);
 });
