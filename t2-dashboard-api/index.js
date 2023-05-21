86cb5563620300f6710deac05cbccd1a5691ee97
viktorkulygin <mrastonone@gmail.com>
2023-05-21 16:22:42 +0300

    routers and interceptors

--- t2-dashboard-api/index.js
+++ t2-dashboard-api/index.js
@@ -1,11 +1,23 @@
 import express from 'express';
+import {userRouter} from './users/users.js';
 
 const port = 8000;
-
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('Request time for main: ', new Date());
+    next();
+});
+
 app.get('/hello', (req, res) => {
-    res.send('Hello from express');
+    throw new Error('My error!');
+});
+
+app.use('/users', userRouter);
+
+app.use((err, req, res, next) => {
+    console.log(err.message);
+    res.status(400).send(err.message);
 });
 
 app.listen(port, () => {
